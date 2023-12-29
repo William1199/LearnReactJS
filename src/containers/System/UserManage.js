f5c7583d4b6a630c291c8a0858e1a7414b653478
William1199 <hieutran1736@gmail.com>
2023-12-29 11:44:41 +0700

    update fe

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,20 +1,78 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import './UserManage.scss';
+import { getAllUsers } from '../../services/userService';
+import ModalUser from './ModalUser';
+
 class UserManage extends Component {
 
-    state = {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            arrUsers: [],
+            isOpenModalUser: false,
+        }
     }
 
-    componentDidMount() {
 
+    async componentDidMount() {
+        let responce = await getAllUsers("ALL");
+        if (responce && responce.errCode === 0) {
+            this.setState({
+                arrUsers: responce.users
+            })
+        }
     }
 
+    handleAddNewUser = () => {
+        alert("click done")
+    }
 
     render() {
+        let arrUsers = this.state.arrUsers;
         return (
-            <div className="text-center">Manage users</div>
+            <div className="user-container">
+                <ModalUser
+                    isOpen={this.state.isOpenModalUser}
+                    test={"abc"}
+                />
+                <div className='title text-center'>Manage Users</div>
+
+                <div className='mx-1'>
+                    <button className='btn btn-primary px-3' onClick={() => this.handleAddNewUser()}>
+                        <i className="fas fa-plus"></i> Add new User</button>
+                </div>
+                <div className='users-table mt-4 mx-1'>
+
+                    <table id="customers">
+                        <tr>
+                            <th>Email</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Address</th>
+                            <th>Action</th>
+                        </tr>
+                        {
+                            arrUsers && arrUsers.map((item, index) => {
+                                return (
+                                    <tr>
+                                        <td>{item.email}</td>
+                                        <td>{item.firstName}</td>
+                                        <td>{item.lastName}</td>
+                                        <td>{item.address}</td>
+                                        <td>
+                                            <button className='btn-edit'><i className="fas fa-pencil-alt"></i></button>
+                                            <button className='btn-delete'><i className="fas fa-trash-alt"></i></button>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </table>
+                </div>
+
+            </div>
         );
     }
 
