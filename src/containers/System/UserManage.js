08e13551af6ebd59cbc53763088895315df228c1
William1199 <hieutran1736@gmail.com>
2024-01-09 11:17:51 +0700

    update delete user and auto refesh create field

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import './UserManage.scss';
-import { getAllUsers, createNewUserService } from '../../services/userService';
+import { getAllUsers, createNewUserService, deleteUserService } from '../../services/userService';
 import ModalUser from './ModalUser';
+import { emitter } from '../../utils/emitter';
 
 class UserManage extends Component {
 
@@ -51,11 +52,25 @@ class UserManage extends Component {
                 this.setState({
                     isOpenModalUser: false
                 })
+                emitter.emit('EVENT_CLEAR_MODAL_DATA', { "id": 'your id' })
             }
         } catch (e) {
             console.log(e)
         }
 
+    }
+
+    handleDeleteUser = async (user) => {
+        try {
+            let res = await deleteUserService(user.id);
+            if (res && res.errCode === 0) {
+                await this.getAllUsersFromReact();
+            } else {
+                alert("")
+            }
+        } catch (e) {
+            console.log(e)
+        }
     }
 
     render() {
@@ -94,7 +109,7 @@ class UserManage extends Component {
                                             <td>{item.address}</td>
                                             <td>
                                                 <button className='btn-edit'><i className="fas fa-pencil-alt"></i></button>
-                                                <button className='btn-delete'><i className="fas fa-trash-alt"></i></button>
+                                                <button className='btn-delete' onClick={() => this.handleDeleteUser(item)}><i className="fas fa-trash-alt"></i></button>
                                             </td>
                                         </tr>
                                     )
--- src/services/userService.js
+++ src/services/userService.js
@@ -9,11 +9,20 @@ const getAllUsers = (inputId) => {
 }
 
 const createNewUserService = (data) => {
-    return axios.post('/api/create-new-user', data)
+    return axios.post('/api/create-new-user', data);
+}
+
+const deleteUserService = (userId) => {
+    return axios.delete('/api/delete-user', {
+        data: {
+            id: userId
+        }
+    });
 }
 
 export {
     handleLoginApi,
     getAllUsers,
     createNewUserService,
+    deleteUserService,
 }
